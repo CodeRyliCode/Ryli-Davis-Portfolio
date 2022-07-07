fa03797618a24b236ecd0ca0c061553249433d80
Ryli Davis <rylidavis94@gmail.com>
2022-07-07 14:42:39 -0700

    Trying to route project ids

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ app.set("view engine", "pug");
 //using express.static method to serve static public files
 app.use(express.static("public"));
 
-/*Setup the development server using the listen method.We have an Express router.
-  This code will create a server and when I run it, the server will run on my machine. And I can
-  send it requests through a special url called localhost.
-  
-  The listen method can take the callback function as a parameter*/
-app.listen(3000, () => {
-  console.log("the application is running on localhost:3000!");
-});
 
 /* I created an "index" route (/) to render the home page with the locals set to data.projects
  */
@@ -32,6 +24,20 @@ app.get("/", (req, res) => {
     res.render("about");
   });
 
+  app.get("/projects:id", (req, res) => {
+    res.locals = { data: 'projects' } ;
+    res.render("project");
+  });
+
+
+/*Setup the development server using the listen method.We have an Express router.
+  This code will create a server and when I run it, the server will run on my machine. And I can
+  send it requests through a special url called localhost.
+  
+  The listen method can take the callback function as a parameter*/
+app.listen(3000, () => {
+  console.log("the application is running on localhost:3000!");
+});
 
 
 /*Dynamic "project" routes (/project/:id or /projects/:id) based on the id of the project that render a customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
